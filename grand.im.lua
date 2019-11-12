977f66e03022c912cd3d53f3f1bb223ad4d8930d
Chris West (Faux) <git@goeswhere.com>
2019-11-12 21:33:05 +0000

    v6

--- grand.im.lua
+++ grand.im.lua
@@ -1,5 +1,11 @@
 tango = "51.75.163.10"
+tango_v6 = "2001:41d0:801:2000:0:0:0:11ca"
+
 a(_a, tango)
 a("*", tango)
+
+aaaa(_a, tango_v6)
+aaaa("*", tango_v6)
+
 caa("", "letsencrypt.org", "issue")
 
